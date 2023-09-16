e725a85bdac3460080f19a39f9827057ff0c1a43
Salman ElHussini <hussainyred@gmail.com>
2023-09-16 09:10:38 +0300

    completed a task

--- variadictest.c
+++ variadictest.c
@@ -11,24 +11,35 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int counter = 0;
+	unsigned int counter = 0;
 	va_list printable_numbers;
 
-	if (!n)
-	{
-		printf("\n");
-		return;
-	}	
 	va_start(printable_numbers, n);
-	
-		while (counter < n)
-		{
-			if (!separator)
-				printf("%d", va_arg(printable_numbers, int));
-			else
-				printf("%d%s\n", separator, va_arg(printable_numbers, int));
-			counter++;
-		}
+
+	while (counter < n)
+	{
+		if (!separator)
+			printf("%d", va_arg(printable_numbers, int));
+		else if (counter < n - 1)
+			printf("%d%s", va_arg(printable_numbers, int), separator);
+		else
+			printf("%d", va_arg(printable_numbers, int));
+
+		counter++;
+	}
+
+	printf("\n");
 	
 	va_end(printable_numbers);
+}
+
+/**
+* main - check the code
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	    print_numbers(", ", 4, 0, 98, -1024, 402);
+	        return (0);
 }
